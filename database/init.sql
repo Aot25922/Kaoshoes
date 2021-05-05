e11de975a27464be22a5fbf006269b481b12b2d2
Thansap Leelapisuth <60164569+Thanasap-L@users.noreply.github.com>
2021-05-05 15:52:28 +0700

    Change Schema

--- database/init.sql
+++ database/init.sql
@@ -1,114 +1,128 @@
 -- MySQL Workbench Forward Engineering
 
-GRANT ALL ON *.* to 'int221'@'%' WITH GRANT OPTION;
-DROP DATABASE IF EXISTS `kaofood`;
-CREATE DATABASE IF NOT EXISTS `kaofood`;
-USE `kaofood` ;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Table `9Food`.`Order`
+-- Schema 9shoes
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Order` ;
+DROP SCHEMA IF EXISTS `9shoes` ;
 
-CREATE TABLE IF NOT EXISTS `Order` (
+-- -----------------------------------------------------
+-- Schema 9shoes
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `9shoes` DEFAULT CHARACTER SET utf8 ;
+USE `9shoes` ;
+
+-- -----------------------------------------------------
+-- Table `9shoes`.`Order`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `9shoes`.`Order` ;
+
+CREATE TABLE IF NOT EXISTS `9shoes`.`Order` (
   `OrderId` INT NOT NULL,
   `CusName` VARCHAR(10) NULL,
-  `Manuface` DATE NOT NULL,
   PRIMARY KEY (`OrderId`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `9Food`.`Category`
+-- Table `9shoes`.`Brand`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Category` ;
+DROP TABLE IF EXISTS `9shoes`.`Brand` ;
 
-CREATE TABLE IF NOT EXISTS `Category` (
-  `CateId` INT NOT NULL,
-  `CateName` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`CateId`))
+CREATE TABLE IF NOT EXISTS `9shoes`.`Brand` (
+  `BrandId` INT NOT NULL,
+  `BrandName` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`BrandId`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `9Food`.`Menu`
+-- Table `9shoes`.`Product`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Menu` ;
+DROP TABLE IF EXISTS `9shoes`.`Product` ;
 
-CREATE TABLE IF NOT EXISTS `Menu` (
-  `MenuId` INT NOT NULL AUTO_INCREMENT,
-  `MenuName` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `9shoes`.`Product` (
+  `ProductId` INT NOT NULL,
+  `ProductName` VARCHAR(45) NOT NULL,
   `Descript` VARCHAR(500) NOT NULL,
-  `Cost` DOUBLE NOT NULL,
+  `Price` DOUBLE NOT NULL,
   `ImagePath` VARCHAR(100) NOT NULL,
-  `CateId` INT NOT NULL,
-  PRIMARY KEY (`MenuId`),
-  INDEX `fk_Menu_Category_idx` (`CateId` ASC) VISIBLE,
+  `ManuDate` DATE NOT NULL,
+  `BrandId` INT NOT NULL,
+  PRIMARY KEY (`ProductId`, `BrandId`),
+  INDEX `fk_Menu_Category_idx` (`BrandId` ASC) VISIBLE,
   CONSTRAINT `fk_Menu_Category`
-    FOREIGN KEY (`CateId`)
-    REFERENCES `Category` (`CateId`)
+    FOREIGN KEY (`BrandId`)
+    REFERENCES `9shoes`.`Brand` (`BrandId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `9Food`.`Orderdetail`
+-- Table `9shoes`.`Orderdetail`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Orderdetail` ;
+DROP TABLE IF EXISTS `9shoes`.`Orderdetail` ;
 
-CREATE TABLE IF NOT EXISTS `Orderdetail` (
-  `OrderId` INT NOT NULL,
-  `MenuId` INT NOT NULL,
-  PRIMARY KEY (`OrderId`, `MenuId`),
-  INDEX `fk_Orderdetail_Order1_idx` (`OrderId` ASC) VISIBLE,
-  INDEX `fk_Orderdetail_Menu1_idx` (`MenuId` ASC) VISIBLE,
+CREATE TABLE IF NOT EXISTS `9shoes`.`Orderdetail` (
+  `OrdertId` INT NOT NULL,
+  `ProductId` INT NOT NULL,
+  PRIMARY KEY (`OrdertId`, `ProductId`),
+  INDEX `fk_Orderdetail_Order1_idx` (`OrdertId` ASC) VISIBLE,
+  INDEX `fk_Orderdetail_Menu1_idx` (`ProductId` ASC) VISIBLE,
   CONSTRAINT `fk_Orderdetail_Order1`
-    FOREIGN KEY (`OrderId`)
-    REFERENCES `Order` (`OrderId`)
+    FOREIGN KEY (`OrdertId`)
+    REFERENCES `9shoes`.`Order` (`OrderId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Orderdetail_Menu1`
-    FOREIGN KEY (`MenuId`)
-    REFERENCES `Menu` (`MenuId`)
+    FOREIGN KEY (`ProductId`)
+    REFERENCES `9shoes`.`Product` (`ProductId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `9Food`.`Size`
+-- Table `9shoes`.`Size`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Size` ;
+DROP TABLE IF EXISTS `9shoes`.`Size` ;
 
-CREATE TABLE IF NOT EXISTS `Size` (
+CREATE TABLE IF NOT EXISTS `9shoes`.`Size` (
   `SizeId` INT NOT NULL,
   `SizeValue` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`SizeId`),
-  UNIQUE INDEX `Size_UNIQUE` (`SizeValue` ASC) VISIBLE)
+  UNIQUE INDEX `SizeValue_UNIQUE` (`SizeValue` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `9Food`.`Menu_has_Size`
+-- Table `9shoes`.`Product_has_Size`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Menu_has_Size` ;
+DROP TABLE IF EXISTS `9shoes`.`Product_has_Size` ;
 
-CREATE TABLE IF NOT EXISTS `Menu_has_Size` (
-  `MenuId` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `9shoes`.`Product_has_Size` (
+  `ProductId` INT NOT NULL,
   `SizeId` INT NOT NULL,
-  `Price`  DOUBLE NOT NULL,
-  PRIMARY KEY (`MenuId`, `SizeId`),
+  PRIMARY KEY (`ProductId`, `SizeId`),
   INDEX `fk_Menu_has_Size_Size1_idx` (`SizeId` ASC) VISIBLE,
-  INDEX `fk_Menu_has_Size_Menu1_idx` (`MenuId` ASC) VISIBLE,
+  INDEX `fk_Menu_has_Size_Menu1_idx` (`ProductId` ASC) VISIBLE,
   CONSTRAINT `fk_Menu_has_Size_Menu1`
-    FOREIGN KEY (`MenuId`)
-    REFERENCES `Menu` (`MenuId`)
+    FOREIGN KEY (`ProductId`)
+    REFERENCES `9shoes`.`Product` (`ProductId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Menu_has_Size_Size1`
     FOREIGN KEY (`SizeId`)
-    REFERENCES `Size` (`SizeId`)
+    REFERENCES `9shoes`.`Size` (`SizeId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
